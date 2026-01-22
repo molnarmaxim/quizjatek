0ba3375addbf5f20e29bf9e9c2222b5867381759
Molnár Maxim <scopsy@icloud.com>
2026-01-22 10:47:58 +0100

    Add initial p.js file with a console log statement

--- main.js
+++ main.js
@@ -3,13 +3,42 @@ let url = window.location.search;
 if(url.includes("?")){
     let params = new URLSearchParams(url);
 
-    if(params.get("q1") === "q1")
-        {
-        document.getElementById("kerdes1").innerHTML = "I.Melyik programnyelvben íródott ez a kód? - <span style='color:green'>helyes valasz</span>";
-        } 
-        else if(params.get("q1"))
-        {
-        document.getElementById("kerdes1").innerHTML = "I.Melyik programnyelvben íródott ez a kód? - <span style='color:red'>helyes valasz</span>";
+    if(params.get("q1") === "Python")
+    {
+        document.getElementById("kerdes1").innerHTML = "I.Melyik programnyelvben íródott ez a kód? - <span style='color:green'>Helyes válasz</span>";
+    } else if(params.get("q1")) 
+    {
+        document.getElementById("kerdes1").innerHTML = "I.Melyik programnyelvben íródott ez a kód? - <span style='color:red'>Helytelen válasz</span>";
+    }
+
+    let masodikvalasz = params.get("q2");
+        if(masodikvalasz){
+            masodikvalasz = masodikvalasz.toLowerCase().trim();
+            if(masodikvalasz === "javaskript" || masodikvalasz === "javascript" || masodikvalasz === "js"){
+                document.getElementById("kerdes2").innerHTML = "II. Melyik nyelvben használjuk ez a arancsot? - <span style='color:green'>Helyes válasz</span>";
+            } else {
+                document.getElementById("kerdes2").innerHTML = "II. Melyik nyelvben használjuk ez a arancsot? - <span style='color:red'>Helytelen válasz (helyes: javescript)</span>";
+            }
+        }
+    
+
+    let szakmaValues = params.getAll("q3");
+        if(szakmaValues.length > 0){
+            let helyes = 0;
+            let rossz = 0;
+            for(let szakma of szakmaValues){
+                if(szakma === "programozas" || szakma === "gepeszet"){
+                    helyes++;
+                } else {
+                    rossz++;
+                }
+            }
+            
+            if(helyes === 2 && rossz === 0){
+                document.getElementById("kerdes2").innerHTML = "2.milyen szakmak vannak a pg-ben - <span style='color:green'>helyes valasz</span>";
+            } else {
+                document.getElementById("kerdes2").innerHTML = "2.milyen szakmak vannak a pg-ben - <span style='color:orange'>reszben helyes (" + helyes + "/2 helyes)</span>";
+            }
         }
 
 
@@ -17,6 +46,9 @@ if(url.includes("?")){
 
 
 
+}
 
 
+function resi() {
+    window.location.href = "index.html";
 }
